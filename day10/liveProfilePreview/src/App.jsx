80b92e956382fb709c722ef7bfd252092f074665
NS9948 <148986126+NS9948@users.noreply.github.com>
2026-06-16 07:28:27 +0530

    Day 10 - React controlled inputs and live profile preview

---
+++ day10/liveProfilePreview/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [name, setName] = useState("");
+  const [course, setCourse] = useState("");
+  const [year, setYear] = useState("");
+
+  
+
+  return (
+    <>
+      <label>Name:</label>
+      <input value={name} onChange={(e) => setName(e.target.value)}/>
+      <label>Course:</label>
+      <input value={course} onChange={(e) => setCourse(e.target.value)}/>
+      <label>Year:</label>
+      <input value={year} onChange={(e) => setYear(e.target.value)}/>
+      <h2>Student Profile</h2>
+      <p>Name: {name}</p>
+      <p>Course: {course}</p>
+      <p>Year: {year}</p>
+    </>
+    
+  )
+}
+
+export default App
